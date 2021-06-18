5ae60012093d503d5b6a3b48bbe397e353126d21
lnbalon <lnbalon87@gmail.com>
2021-06-19 07:47:10 +0800

    use of while loop

--- codes/temp-converter.c
+++ codes/temp-converter.c
@@ -1,17 +1,26 @@
 #include <stdio.h>
 
+#define LOWER 0
+#define UPPER 300
+#define STEP 20
+
+
+float cel_fahr_while () {
+    printf("temperature converter using while loop...")
+    float cel, fahr;
+    fahr = LOWER;
+    while (fahr <= UPPER) {
+        cel = (5.0/9.0) * (fahr-32.0);
+        printf("%3.0f %6.1f\n", fahr, cel);
+        fahr = fahr + STEP;
+    }
+}
+
+
 int main (){
 
-    int fahr, cel;
-    int start, stop, increment;
-    start = 1;
-    stop = 10;
-    increment = 1;
-    while (start <= stop){
-        printf("%d\n", start);
-        start = start + increment;
+    cel_fahr_while();
 
-
-    }
+    
     return 0;
 }
