0f23c08d6be146081ffe72daf6102c1553daad67
lnbalon <lnbalon87@gmail.com>
2021-08-12 19:09:31 +0800

    checkpoint: need to study hash table in c

--- codes/birthday.c
+++ codes/birthday.c
@@ -1,26 +1,30 @@
 #include<stdio.h>
 #include<stdlib.h>
 
-int UPPER = 10;
+int UPPER = 365;
 int LOWER = 1;
-int NUM_OF_RUNS = 100;
+int NUM_OF_RUNS = 20;
 
+int generate_random_number(int lower, int upper) {
 
-int generate_random(int lower, int upper) {
+    // Function to generate random integer from lower to upper
+
     return (rand() % (upper - lower + 1)) + lower;
 }
 
 
 
-
-
 int main () {
     
+    int i,j;
+    int n[NUM_OF_RUNS];
 
-
-    }
+    for (i=0; i<NUM_OF_RUNS; i++)
+        n[i] = generate_random_number(LOWER, UPPER);
     
-
+    for (j=0; j<NUM_OF_RUNS; j++)
+        printf("Element[%d] = %d\n", j, n[j]);
 
     return 0;
+
 }
