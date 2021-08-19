724d8d839de9b64846018c55816cf36f0d6c7d88
lnbalon <lnbalon87@gmail.com>
2021-08-19 19:29:04 +0800

    fixed error

--- codes/enum.c
+++ codes/enum.c
@@ -5,9 +5,9 @@ int main () {
 
     enum company {GOOGLE, FACEBOOK, XEROX, YAHOO, EBAY, MICROSOFT};
 
-    int c1 = GOOGLE;
-    int c2 = XEROX;
-    int c3 = EBAY;
+    enum company c1 = GOOGLE;
+    enum company c2 = XEROX;
+    enum company c3 = EBAY;
 
     printf("%d\n", c1);
     printf("%d\n", c2);
