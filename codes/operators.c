6ee9ca3c8255cb1f05b3951b94d3f82c69305a34
lnbalon <lnbalon87@gmail.com>
2021-08-20 18:14:45 +0800

    bitwise operators

--- codes/operators.c
+++ codes/operators.c
@@ -2,20 +2,11 @@
 
 int main() {
 
-    int a = 12, b = 14;
+    
 
-    int result;
-    result = a % b;
     printf("%d\n", result);
     printf("%d\n", a++);
     printf("%d\n", ++a);
 
-    _Bool c = 1;
-    _Bool d = 0;
-    _Bool new_result;
 
-    new_result = c && d;
-
-    printf("%d\n", new_result);
-    printf("%d\n", !(c&&d));
 }
