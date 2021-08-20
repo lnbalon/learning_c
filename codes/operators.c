88aaffea6f909d2a994e0dc6abf033a9d1a6d8d7
lnbalon <lnbalon87@gmail.com>
2021-08-20 18:12:58 +0800

    logical operators

--- codes/operators.c
+++ codes/operators.c
@@ -7,5 +7,15 @@ int main() {
     int result;
     result = a % b;
     printf("%d\n", result);
+    printf("%d\n", a++);
+    printf("%d\n", ++a);
 
+    _Bool c = 1;
+    _Bool d = 0;
+    _Bool new_result;
+
+    new_result = c && d;
+
+    printf("%d\n", new_result);
+    printf("%d\n", !(c&&d));
 }
