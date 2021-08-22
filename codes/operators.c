933a970b6e7218f40ed0422f986020b59206780f
lnbalon <lnbalon87@gmail.com>
2021-08-22 19:19:22 +0800

    done with first exercise

--- codes/operators.c
+++ codes/operators.c
@@ -1,13 +1,17 @@
 #include <stdio.h>
 
-int main() {
+ int main() {
 
-    
-    
 
-    printf("%d\n", );
-    printf("%d\n", );
-    printf("%d\n", ;
+
+    float minutes, days, years; 
+    printf("Enter the number of minutes:");
+    scanf("%f", &minutes);
+
+    days = minutes / (60*24);
+    years = days / 365;
+    printf("the number of days are %.2f\n", days);
+    printf("the number of years are %.2f\n", years);
 
 
 }
