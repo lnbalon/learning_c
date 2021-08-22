24994995543a48f548cf649d71612e0938f0bf63
lnbalon <lnbalon87@gmail.com>
2021-08-22 19:28:35 +0800

    byte size

---
+++ codes/byte-size.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+
+int main () {
+
+
+
+    printf("char: %u\n", sizeof(char));
+    printf("short: %lu\n", sizeof(short));
+    printf("int: %lu\n", sizeof(int));
+    printf("long: %lu\n", sizeof(long));
+    printf("long long: %lu\n", sizeof(long long));
+    printf("float: %lu\n", sizeof(float));
+    printf("double: %lu\n", sizeof(double));
+    printf("long double: %lu\n", sizeof(long double));
+
+}
