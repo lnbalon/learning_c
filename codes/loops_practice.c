d3fe3a3705ff6f48a4058661f1f6651438d9b662
lnbalon <lnbalon87@gmail.com>
2021-09-13 10:48:41 +0800

    finalized

--- codes/loops_practice.c
+++ codes/loops_practice.c
@@ -1,25 +1,57 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
 
+
+#define LOWER 1
+#define UPPER  20
+#define NUMBER_TRIES 5
 
 int main () {
+    
+    printf("Guess the fucking number!\n");
+    printf("Select only numbers from zero to twenty (0 to 20). Capisce? \n");
 
-    for (int i=1, j=2; i<=5; i++, j = j+2)
-        printf("%d, %d \n", i, j);
+    srand(time(0));
+    int num = (rand() % (LOWER - UPPER + 1)) + LOWER;
+    printf("%d\n", num);
 
+    int i=0;
 
-    int looper_condition = 1;
+    while (i<NUMBER_TRIES){
 
-
-
-    while (looper_condition<5) {
-        looper_condition++;
-        printf("entered and exited a while loop\n");
-    }
-    int counter =1;
-    do {
+        int guess;
+        printf("You have %d guess left!\n", NUMBER_TRIES - (i));
+        printf("Enter a number: ");
+        scanf("%d", &guess);
         
-        counter++;
-        printf("print this shit\n");
-    } while (counter<10);
+        if (!(1<=guess & guess<=20)){
+            printf("Enter a valid number you idiot! You lose one try.\n");
+            i++;
+            continue;
+        }
 
-}
+        else if (num < guess){
+            printf("The number is less than your guess\n");
+        }
+
+        else if (num > guess){
+            printf("The number is greater than your guess!\n");
+        }
+
+        else {
+            printf("You Guessed Correctly! %d is equal to %d\n", guess, num);
+            printf("You Win!\n");
+            break;
+        }
+
+        i++;
+        if (i == NUMBER_TRIES)
+            printf("You Lose!\n");
+        
+    }
+
+
+
+
+    }
